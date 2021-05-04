3842a928aa25f8803f2b82b9005e5313cc327bf2
rejnev <mohammad_bagherani@yahoo.com>
2021-05-04 18:24:41 +0430

    source updated

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,84 @@
+const ServerSocket = require('socket.io').Server;
+
+var io = new ServerSocket({
+  cors: { origin: '*' }
+})
+io.listen(3004);
+
 var express = require('express');
+const { querySymbol } = require('../services/coinex');
 var router = express.Router();
-
 const coinex = require('../services/coinex')
-var deals = {};
-var states = {};
 
-coinex.on('deals.update', data => {
-  deals = data;
+var symbols = [];
+var marketInterval = 0;
+var items = {};
+
+coinex.getMarketSymbols();
+
+coinex.on('symbols', data => {
+  symbols = data;
+
+  data.forEach(symbol => {
+    items[symbol] = {};
+  });
 })
 
-coinex.on('state.update', data => {
-  states = data;
+coinex.on('connected', () => {
+  if (marketInterval)
+    clearInterval(marketInterval);
+
+  var counter = 0;
+  marketInterval = setInterval(() => {
+
+    if (symbols.length > 0) {
+      var symbol = symbols[counter];
+
+      coinex.querySymbol(symbol, 900);
+
+      setTimeout(() => {
+        coinex.querySymbol(symbol, 1200);
+      }, 500);
+
+      setTimeout(() => {
+        coinex.querySymbol(symbol, 3600);
+      }, 1000);
+
+      setTimeout(() => {
+        coinex.querySymbol(symbol, 7200);
+      }, 1500);
+
+      setTimeout(() => {
+        coinex.querySymbol(symbol, 86400);
+      }, 2000);
+    }
+
+    counter++;
+    if (counter > symbols.length)
+      counter = 0;
+
+  }, 2500);
+})
+
+
+coinex.on('symbolResult', res => {
+  var { symbol, result: data } = res;
+  Object.keys(data)
+    .forEach(key => {
+      items[symbol][key + data.period] = data[key];
+    })
+
+  io.emit('data', items);
 })
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express', deals: JSON.stringify(deals), states: JSON.stringify(states) });
+  res.render('index', { title: 'Crypto Dashboard' });
 });
+
+router.get('/api/symbols', function (req, res, next) {
+  res.send({ success: true, data: symbols });
+});
+
 
 module.exports = router;
--- src/services/coinex.js
+++ src/services/coinex.js
@@ -9,54 +9,97 @@ class Coinex extends EventEmitter {
      */
     symbols;
 
+    IDs;
+
     constructor() {
         super();
+
+        this.IDs = {};
 
         this._client = new WebSocket('wss://socket.coinex.com/', {
             perMessageDeflate: false,
             handshakeTimeout: 15000,
         });
 
-        this._client.on('open', (ex) => { this._subscribeToCoinex(ex) });
+        this._client.on('open', (ex) => { this._checkConnection(ex) });
 
         this._client.on('message', (data) => {
             const response = JSON.parse(data);
-
-            this.emit(response.method, response.params);
+            if (!response.error)
+                this.emit("symbolResult", { symbol: this.IDs[response.id], result: response.result });
         });
     }
 
-    _subscribeToCoinex(err) {
+    getMarketSymbols() {
+        axios.default.get('https://api.coinex.com/v1/market/list').then((resp) => {
+            var data = resp.data.data.filter(x => x.indexOf("USDT") > -1);
+            this.emit('symbols', data)
+        }).catch(ex => {
+            console.log('error getting symbols list', ex);
+            this.emit('symbols', [])
+        })
+    }
+
+    querySymbol(symbol, seconds) {
+        var id = Object.keys(this.IDs).length + 1;
+        if (this.IDs[id] == undefined)
+            this.IDs[id] = symbol;
+        this._client.send(JSON.stringify({
+            "method": "state.query",
+            "params": [symbol, seconds],
+            "id": id
+        }));
+    }
+
+    _checkConnection(err) {
         if (err) {
             console.log('error in connecting to coinex websocket server.');
             console.log('try reconnecting in 5 seconds...');
             setTimeout(() => {
-                this._client.on('open', (ex) => { this._subscribeToCoinex(ex) });
+                this._client.on('open', (ex) => { this._checkConnection(ex) });
             }, 5000);
         }
-        else {
-            this._client.send(JSON.stringify({
-                "method": "state.subscribe",
-                "params": [],
-                "id": 1
-            }));
-
-            axios.default.get('https://api.coinex.com/v1/market/list').then((resp) => {
-                this.symbols = resp.data.data;
-
-                this._client.send(JSON.stringify({
-                    "method": "deals.subscribe",
-                    "params": this.symbols,
-                    "id": 2,
-                }))
-            }).catch(ex => {
-                console.log('error getting symbols list', ex);
-                this.symbols = [];
-            })
-
-
-        }
+        else
+            this.emit('connected');
     }
+
+    // _subscribeToCoinex(err) {
+    //     if (err) {
+    //         console.log('error in connecting to coinex websocket server.');
+    //         console.log('try reconnecting in 5 seconds...');
+    //         setTimeout(() => {
+    //             this._client.on('open', (ex) => { this._subscribeToCoinex(ex) });
+    //         }, 5000);
+    //     }
+    //     else {
+    //         this._client.send(JSON.stringify({
+    //             "method": "state.subscribe",
+    //             "params": [],
+    //             "id": 1
+    //         }));
+
+    //         this._client.send(JSON.stringify({
+    //             "method": "state.query",
+    //             "params": ["CETUSDT", 3600],
+    //             "id": 1001
+    //         }));
+
+    //         axios.default.get('https://api.coinex.com/v1/market/list').then((resp) => {
+    //             this.symbols = resp.data.data;
+
+    //             this._client.send(JSON.stringify({
+    //                 "method": "deals.subscribe",
+    //                 "params": this.symbols,
+    //                 "id": 2,
+    //             }))
+    //         }).catch(ex => {
+    //             console.log('error getting symbols list', ex);
+    //             this.symbols = [];
+    //         })
+
+
+    //     }
+    // }
 }
 
 
